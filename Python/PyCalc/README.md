649c604c7a98c47d50bd7f20b30a7ed9f6d37113
rska <bp4fradek@gmail.com>
2021-01-07 17:32:59 +0100

    Repo refactoring - PyCalc

--- Python/PyCalc/README.md
+++
@@ -1,4 +0,0 @@
-Simple calculator created with PyQt5 framework.
-
-Screenshot below:
-![](images/pycalc.png)
--- Python/PyCalc/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from calc import MainWindow
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-window.setFixedSize(460, 240)
-sys.exit(app.exec_())
